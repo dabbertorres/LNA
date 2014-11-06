a5e3fbc8c623133375761512e88b22f1595a1f90
Alec Iverson <adiverso93@gmail.com>
2014-11-05 23:47:12 -0700

    simple lightuserdata test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 #include "LuaCpp/LuaCpp.hpp"
 
+template<typename Type>
+struct Vector2D
+{
+	Type x, y;
+};
+
 int main(int argc, char **argv)
 {
 	lpp::State luaState;
@@ -16,7 +22,15 @@ int main(int argc, char **argv)
 	luaState["j"] = std::vector<int>{5, 3, 2};
 	luaState["m"] = std::map<std::string, std::string>{{"first", "hello"}, {"second", "world"}};
 	
+	Vector2D<int> vec2d{5, 3};
+	luaState["vec2d"] = vec2d;
+	
 	luaState.run();
+	
+	Vector2D<int>* anotherVec2d = luaState["vec2d"];
+	
+	std::cout << "vec2d.x: " << anotherVec2d->x << '\n';
+	std::cout << "vec2d.y: " << anotherVec2d->y << '\n';
 	
 	std::string str0 = luaState["i"];
 	std::string str1 = luaState["j"][1];

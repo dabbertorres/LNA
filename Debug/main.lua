bd6d78eadd6046cfe1cdae53fc2d3d92b42acacb
Alec Iverson <adiverso93@gmail.com>
2014-11-08 23:33:11 -0700

    Playing around, more tests (C++ functions!)

--- Debug/main.lua
+++ Debug/main.lua
@@ -10,10 +10,20 @@ nums = {3, 2, 1}
 
 map = {hi = 5, world = 3}
 
+print("add: "..add(1, 3))
+
 print(m.first)
 print(m.second)
 
 function printSomething(x, y)
 	print("x: "..x)
 	print("y: "..y)
+end
+
+function sayHello()
+	print("hello!")
+end
+
+function return2Nums()
+	return 5, 3
 end
--- main.cpp
+++ main.cpp
@@ -8,30 +8,44 @@ struct Vector2D
 	Type x, y;
 };
 
+int add(int x, int y)
+{
+	return x + y;
+}
+
 int main(int argc, char **argv)
 {
-	lpp::State luaState;
+	lpp::State luaState("test");
 	luaState.openLib("base", luaopen_base);
 	luaState.openLib("math", luaopen_math);
 	luaState.openLib("string", luaopen_string);
 	luaState.openLib("table", luaopen_table);
 	
+	// load a file
 	luaState.loadFile("main.lua");
 	
+	// assign a Lua variable to C++ function
+	luaState["add"] = static_cast<std::function<int(int, int)>>(add);
+	
+	// assign values to Lua variables
 	luaState["i"] = 9;
 	luaState["j"] = std::vector<int>{5, 3, 2};
 	luaState["m"] = std::map<std::string, std::string>{{"first", "hello"}, {"second", "world"}};
 	
+	// assign C++ object to Lua variable
 	Vector2D<int> vec2d{10, 4};
 	luaState["vec2d"] = vec2d;
 	
+	// run the file
 	luaState.run();
 	
+	// C++ object set from Lua variable
 	Vector2D<int>* anotherVec2d = luaState["vec2d"];
 	
 	std::cout << "vec2d.x: " << anotherVec2d->x << '\n';
 	std::cout << "vec2d.y: " << anotherVec2d->y << '\n';
 	
+	// Get value of Lua variables
 	std::string str0 = luaState["i"];
 	std::string str1 = luaState["j"][1];
 	std::string str2 = luaState["j"][2];
@@ -46,13 +60,18 @@ int main(int argc, char **argv)
 	
 	std::map<std::string, int> strNums = luaState["map"];
 	
-	luaState["printSomething"](1, 7, 2);
-	
 	for(auto& n : nums)
 		std::cout << "Num: " << n << '\n';
 		
 	for(auto& it : strNums)
 		std::cout << it.first << ": " << it.second << '\n';
+	
+	// call lua function
+	luaState["printSomething"](1, 7);
+	luaState["sayHello"]();
+	
+	int one, two;
+	//std::tie(one, two) = luaState["return2Nums"]();
 	
 	return 0;
 }
